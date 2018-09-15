bf72bc4b05e277b57684b930918c677c801c8112
M150 <stevovic@gmail.com>
2018-09-15 12:10:19 +0200

    switch to workbox

--- razzle.config.js
+++ razzle.config.js
@@ -1,30 +1,50 @@
-const modifyBuilder = require('razzle-plugin-pwa').default;
+const { GenerateSW } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const pwaConfig = {
-  swDest: 'sw.js',
-  clientsClaim: true,
-  skipWaiting: true
-};
-
-const manifestConfig = {
-  filename: 'manifest.json',
-  name: 'Sigma',
-  short_name: 'Sigma',
-  description: 'custom Excel doc web presentation',
-  display: 'standalone',
-  start_url: '.',
-  theme_color: '#000000',
-  background_color: '#303030',
-  icons: [
-    {
-      src: path.resolve('public/icon.png'),
-      sizes: [96, 128, 192, 256, 384, 512]
-    }
-  ]
-};
-
-const modify = modifyBuilder({ pwaConfig, manifestConfig });
 
 module.exports = {
-  plugins: [{ func: modify }]
+  modify: (config, { target }) =>
+    target === 'web'
+      ? {
+          ...config,
+          plugins: [
+            ...config.plugins,
+            new GenerateSW({
+              clientsClaim: true,
+              skipWaiting: true,
+              runtimeCaching: [
+                {
+                  urlPattern: /images/,
+                  handler: 'cacheFirst'
+                },
+                {
+                  urlPattern: new RegExp(
+                    '^https://fonts.(?:googleapis|gstatic).com/(.*)'
+                  ),
+                  handler: 'cacheFirst'
+                },
+                {
+                  urlPattern: /.*/,
+                  handler: 'networkFirst'
+                }
+              ]
+            }),
+            new WebpackPwaManifest({
+              name: 'Sigma',
+              short_name: 'Sigma',
+              description: 'Custom Excel doc web presentation.',
+              theme_color: '#000000',
+              background_color: '#303030',
+              inject: false,
+              fingerprints: false,
+              icons: [
+                {
+                  src: path.resolve('public/icon.png'),
+                  sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+                }
+              ]
+            })
+          ]
+        }
+      : config
 };
